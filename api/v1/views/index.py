9a041dca5a9f24ad14079535a6566701a4b919be
Lonzo Rust <rustlonzo@gmail.com>
2021-06-23 01:28:56 +0000

    Added stats to index.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,29 @@
 """module for the status route definition"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """returns count of all the classes"""
+    stats_cnt = {
+        'states': storage.count(State),
+        'cities': storage.count(City),
+        'amenities': storage.count(Amenity),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'users': storage.count(User)
+    }
+    return jsonify(stats_cnt)
