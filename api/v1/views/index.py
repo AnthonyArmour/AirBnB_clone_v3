ba5561ccd864f18e45e85b3d6932e1ea84d9f386
Lonzo Rust <rustlonzo@gmail.com>
2021-06-23 01:13:55 +0000

    Corrected importing errors.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """module for the status route definition"""
 from api.v1.views import app_views
-import jsonify
+from flask import jsonify
 
 @app_views.route('/status', strict_slashes=False)
 def status():
