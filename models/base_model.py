d440c3c91f805a8cf7555c03748d639dfc7b9322
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-24 20:27:26 +0000

    fixed hash for users passwords

--- models/base_model.py
+++ models/base_model.py
@@ -76,8 +76,7 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         if "password" in new_dict and getenv("HBNB_TYPE_STORAGE") == "db":
-            if new_dict.pop('password', None) is not None:
-                del new_dict['password']
+            del new_dict['password']
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
