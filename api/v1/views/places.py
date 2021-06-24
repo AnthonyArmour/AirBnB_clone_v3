717698137d7a9717605567ab8946c01143297a6b
AnthonyArmour <anthonydarmour@gmail.com>
2021-06-24 04:48:04 +0000

    fixed places post request method.

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, abort
 from models.city import City
 from models.place import Place
+from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
